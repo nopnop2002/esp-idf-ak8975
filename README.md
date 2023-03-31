e3990b5d870884f3262515fb0a642e9afc1055cf
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-04-01 08:32:26 +0900

    First Release

--- README.md
+++ README.md
@@ -1,2 +1,120 @@
 # esp-idf-ak8975
-Display the orientation of AK8975 with ESP32
+Display the orientation of AK8975 with ESP32.
+
+First, find the offset value for each axis.   
+As you can see, the X and Y axes are quite off-center.   
+![ak8975-calib-1](https://user-images.githubusercontent.com/6020549/229248487-4ef39253-7bc9-4c86-a70f-0af2390a5973.jpg)
+
+
+
+And display the orientation.
+![ak8975-heading](https://user-images.githubusercontent.com/6020549/229248493-17fb6543-8c72-4a39-bebd-cfd7e292426c.jpg)
+
+
+
+# Software requiment
+ESP-IDF V4.4/V5.0.   
+ESP-IDF V5 is required when using ESP32-C2.   
+
+
+# Hardware requirements
+AK8975 3-axis Electronic Compass   
+
+
+# Wireing
+|AK8975||ESP32|ESP32-S2/S3|ESP32-C2/C3||
+|:-:|:-:|:-:|:-:|:-:|:-:|
+|VCC|--|3.3V|3.3V|3.3V||
+|GND|--|GND|GND|GND||
+|SCL|--|GPIO22|GPIO12|GPIO5|(*1)|
+|SDA|--|GPIO21|GPIO11|GPIO4|(*1)|
+|SO|--|N/C|N/C|N/C||
+|CSB|--|3.3V|3.3V|3.3V|use i2c|
+|DRDY|--|N/C|N/C|N/C||
+|CAD1(CADY)|--|GND|GND|GND|(*2)|
+|CAD0|--|GND|GND|GND|(*2)|
+
+(*1)You can change it to any pin using menuconfig.   
+
+(*2)Choosing an i2c address.
+|CAD1|CAD0|Address|
+|:-:|:-:|:-:|
+|GND|GND|0x0C|
+|GND|3.3V|0x0D|
+|3.3V|GND|0x0E|
+|3.3V|3.3V|0x0E|
+
+
+# Caribration
+
+```
+git clone https://github.com/nopnop2002/esp-idf-ak8975
+cd esp-idf-ak8975/calibrate
+idf.py set-target {esp32/esp32s2/esp32s3/esp32c2/esp32c3}
+idf.py menuconfig
+idf.py flash
+```
+
+
+### Configuration   
+To find the offset value, set the compass offset to 0.   
+![config-top](https://user-images.githubusercontent.com/6020549/229249348-21ca8f80-e976-4ddb-8bca-435c475a3290.jpg)
+![config-app](https://user-images.githubusercontent.com/6020549/229249346-0da21399-9640-4708-bdb6-beed7549d55a.jpg)
+
+### Execute calibration   
+ESP32 acts as a web server.   
+I used [this](https://github.com/Molorius/esp32-websocket) component.   
+This component can communicate directly with the browser.   
+It's a great job.   
+Enter the following in the address bar of your web browser.   
+```
+http:://{IP of ESP32}/
+or
+http://esp32.local/
+```
+
+As you move the IMU it plots the X, Y and Z values.   
+X, Y, Z offset are displayed.   
+
+![ak8975-calib-1](https://user-images.githubusercontent.com/6020549/229249418-fb4431ed-ba92-42ba-9964-71711b448361.jpg)
+
+### Execute calibration again   
+If you set the offset you got from the calibration and run it again, the circle position will change.   
+
+![ak8975-calib-2](https://user-images.githubusercontent.com/6020549/229249457-0ef43ec7-b7a6-42a4-bb99-2e4ffa76533c.jpg)
+
+
+
+
+# Display the orientation   
+
+
+
+```
+git clone https://github.com/nopnop2002/esp-idf-ak8975
+cd esp-idf-ak8975/heading
+idf.py set-target {esp32/esp32s2/esp32s3/esp32c2/esp32c3}
+idf.py menuconfig
+idf.py flash
+```
+
+
+### Configuration   
+To find the offset value, set the compass offset to 0.   
+![config-top](https://user-images.githubusercontent.com/6020549/229249348-21ca8f80-e976-4ddb-8bca-435c475a3290.jpg)
+![config-app](https://user-images.githubusercontent.com/6020549/229249346-0da21399-9640-4708-bdb6-beed7549d55a.jpg)
+
+
+### View orientation   
+ESP32 acts as a web server.   
+I used [this](https://github.com/Molorius/esp32-websocket) component.   
+This component can communicate directly with the browser.   
+It's a great job.   
+Enter the following in the address bar of your web browser.   
+```
+http:://{IP of ESP32}/
+or
+http://esp32.local/
+```
+
+![ak8975-heading](https://user-images.githubusercontent.com/6020549/229249937-d7a7d340-b154-4414-a3c5-1a3f2c3795db.jpg)
---
+++ heading/main/ak8975.cpp
@@ -0,0 +1,133 @@
+#include <cstring>
+#include <math.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/message_buffer.h"
+#include "esp_timer.h"
+#include "esp_log.h"
+#include "esp_err.h"
+#include "driver/i2c.h"
+#include "cJSON.h"
+
+extern MessageBufferHandle_t xMessageBufferToClient;
+
+static const char *TAG = "MAG";
+
+// I2Cdev and AK9875 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include "AK8975.h"
+
+#define RAD_TO_DEG (180.0/M_PI)
+#define DEG_TO_RAD 0.0174533
+
+#if 0
+// Arduino macro
+#define micros() (unsigned long) (esp_timer_get_time())
+#define delay(ms) esp_rom_delay_us(ms*1000)
+#endif
+
+AK8975 mag(CONFIG_I2C_ADDR);
+
+// MAG Data Sensitivity adjustment data
+// Sensitivity adjustment data for each axis is stored to fuse ROM on shipment.
+uint8_t MagAdjustment[3];
+float MagCalibration[3];
+
+void ak8975(void *pvParameters){
+	// Initialize device
+	mag.initialize();
+	
+	if (!mag.testConnection()) {
+		ESP_LOGE(TAG, "testConnection fail");
+		vTaskDelete(NULL);
+	}
+
+	// Goto Powerdown Mode
+	mag.setMode(AK8975_MODE_POWERDOWN);
+	// After power-down mode is set, at least 100ms(Twat) is needed before setting another mode.
+	delay(200);
+  
+	// Goto Fuse ROM access mode
+	mag.setMode(AK8975_MODE_FUSEROM);
+	delay(200);
+  
+	// Get Sensitivity adjustment value from fuse ROM
+	// Sensitivity adjustment values for each axis are written in the fuse ROM at the time of shipment.
+	MagAdjustment[0] = mag.getAdjustmentX();
+	MagAdjustment[1] = mag.getAdjustmentY();
+	MagAdjustment[2] = mag.getAdjustmentZ();
+	ESP_LOGI(TAG, "MagAdjustment: %d %d %d", MagAdjustment[0], MagAdjustment[1], MagAdjustment[2]);
+  
+	// Calculate sensitivity
+	// from datasheet 8.3.11
+	for (int i=0;i<3;i++) {
+	  MagCalibration[i] = (float)(MagAdjustment[i] - 128)/256.0f + 1.0f;
+	  ESP_LOGI(TAG, "MagCalibration[%d]=%f", i, MagCalibration[i]);
+	}
+  
+	// Goto Powerdown Mode
+	mag.setMode(AK8975_MODE_POWERDOWN);
+	delay(200);
+  
+	while(1){
+		// Goto Single Mode
+		mag.setMode(AK8975_MODE_SINGLE);
+	
+		// Wait fo Data Ready
+		int counter = 0;
+		while(1) {
+			if (mag.getDataReady()) break;
+			counter++;
+			vTaskDelay(1);
+		}
+		ESP_LOGD(TAG, "counter=%d", counter);
+
+		// Read Measurement data
+		int16_t mx, my, mz; 
+		mag.getHeading(&mx, &my, &mz);
+		bool OverflowStatus = mag.getOverflowStatus();
+		bool DataError = mag.getDataError();
+		ESP_LOGD(TAG, "OverflowStatus=%d DataError=%d", OverflowStatus, DataError);
+		if (OverflowStatus) {
+			ESP_LOGW(TAG, "Magnetic sensor overflow occurred");
+		} else if (DataError) {
+			ESP_LOGW(TAG, "Data read error occurred");
+		} else {
+			ESP_LOGD(TAG, "mag=%d %d %d", mx, my, mz);
+			mx = mx + CONFIG_MAGX;
+			my = my + CONFIG_MAGY;
+			mz = mz + CONFIG_MAGZ;
+			float _mx, _my, _mz; 
+			_mx = (float)mx * MagCalibration[0];
+			_my = (float)my * MagCalibration[1];
+			_mz = (float)mz * MagCalibration[2];
+			ESP_LOGD(TAG, "mag=%f %f %f", _mx, _my, _mz);
+
+			float heading = atan2((double)_my, (double)_mx) * 180.0/3.14159265 + 180;
+			while (heading < 0) heading += 360;
+			while (heading > 360) heading -= 360;
+			ESP_LOGI(TAG, "heading=%f", heading);
+
+			// Send WEB request
+			cJSON *request;
+			request = cJSON_CreateObject();
+			cJSON_AddStringToObject(request, "id", "data-request");
+			cJSON_AddNumberToObject(request, "heading", heading);
+			char *my_json_string = cJSON_Print(request);
+			ESP_LOGD(TAG, "my_json_string\n%s",my_json_string);
+			size_t xBytesSent = xMessageBufferSend(xMessageBufferToClient, my_json_string, strlen(my_json_string), 100);
+			if (xBytesSent != strlen(my_json_string)) {
+				ESP_LOGE(TAG, "xMessageBufferSend fail");
+			}
+			cJSON_Delete(request);
+			cJSON_free(my_json_string);
+		}
+
+		vTaskDelay(10);
+	}
+
+	// Never reach here
+	vTaskDelete(NULL);
+}
